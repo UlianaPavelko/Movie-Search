68b34a7f982ab37123264fb54032cae9f148b2c9
Uliana Pavelko <upavelko@gmail.com>
2018-11-16 14:34:10 +0000

    added movie search service

---
+++ src/app/core/movie-search.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieSearchService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  public searchMovies(searchParam: string): Observable<any> {
+    return this.httpClient
+      .get<any>(`http://www.omdbapi.com/?apikey=3f29742a&t=${searchParam}`);
+  }
+}
