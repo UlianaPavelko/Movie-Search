d282dd72099aba64690c3b45b0e370316b9768b9
Uliana Pavelko <upavelko@gmail.com>
2018-11-16 14:35:17 +0000

    separated core and shared dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { CoreModule } from 'src/app/core/core.module';
+import { MovieSearchModule } from 'src/app/movie-search/movie-search.module';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
     CoreModule,
     SharedModule,
+    AppRoutingModule,
+    MovieSearchModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,21 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { throwIfAlreadyLoaded } from './module-import-guard';
+import { MovieSearchService } from 'src/app/core/movie-search.service';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule
+  ],
+  providers: [MovieSearchService],
+  exports: []
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
