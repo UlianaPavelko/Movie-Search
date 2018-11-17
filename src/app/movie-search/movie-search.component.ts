a5552c8664c343da15e7ab948ffeb56a24283e97
Uliana Pavelko <upavelko@gmail.com>
2018-11-17 14:31:11 +0000

    added search element with store

--- src/app/movie-search/movie-search.component.ts
+++ src/app/movie-search/movie-search.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieSearchService } from 'src/app/core/movie-search.service';
+import { Observable } from 'rxjs';
+import { Store, select } from '@ngrx/store';
+import { Movie } from '../store/movie';
+import * as MovieActions from '../store/actions';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-movie-search',
@@ -7,16 +12,39 @@ import { MovieSearchService } from 'src/app/core/movie-search.service';
   styleUrls: ['./movie-search.component.scss']
 })
 export class MovieSearchComponent implements OnInit {
-  private movies: Object;
+  private movies: Observable<Movie[]>;
+  movieSearchCtrl: FormControl;
 
-  constructor(private movieSearchService: MovieSearchService) { }
+  constructor(private movieSearchService: MovieSearchService, private store: Store<any>) {
+    this.movies = this.store.select('movie');
+  }
 
   ngOnInit() {
-    this.movieSearchService.searchMovies('Avatar').subscribe(data => {
-      this.movies = data;
-      console.log(this.movies);
-    }
-    );
+    this.movieSearchCtrl = new FormControl();
+
+    this.movieSearchCtrl.valueChanges.subscribe(
+      (mode: string) => {
+        console.log(mode);
+        this.removeMovie();
+        this.movieSearchService.searchMovies(mode).subscribe(data => {
+          if (data.Search === undefined) {
+            return;
+          }
+          for (let i = 0; i < data.Search.length; i++) {
+            this.addToMovies(data.Search[i]);
+          }
+        }
+        );
+      });
+  }
+
+
+  private addToMovies(movie: Movie) {
+    this.store.dispatch(new MovieActions.AddMovie(movie));
+  }
+
+  private removeMovie() {
+    this.store.dispatch(new MovieActions.RemoveMovie());
   }
 
 }
